86921701b13b60d636872b47af2670d026c6bd3e
Herfando <herfando_91@yahoo.com>
2025-09-17 16:03:51 +0700

    create Flow_process

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Achievement from "./components/container/Achievement";
 import Brands from "./components/container/Brands";
-import Hero from "./components/container/HeroSection";
+import Flow_process from "./components/container/Flow_process/Flow_process";
+import Hero from "./components/container/Hero";
 import Navbar from "./components/container/Navbar";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Brands />
       <Achievement />
+      <Flow_process />
     </>
   );
 }
