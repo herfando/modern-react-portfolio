b82cf0fe63d458bfd22f9bc8b6a57c194573c946
Herfando <herfando_91@yahoo.com>
2025-09-19 20:29:00 +0700

    edit responsive navbar, hero, brands, our process

--- src/components/container/02_Hero/Hero.tsx
+++ src/components/container/02_Hero/Hero.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import Button from "../../ui/Button/Button";
-import DarkModeToggle from "../../ui/DarkModeToggle/DarkModeToggle";
 
-
-const Hero = () => {
+const Hero: React.FC = () => {
   return (
-    <section className="h-[731px] bg-white dark:bg-black text-black dark:text-white grid grid-cols-1 md:grid-cols-2 md:h-[663px] translate-y-16 md:translate-y-0 py-10 md:py-0">
-      {/* Article dan button */}
-      <article className="p-4 w-full md:translate-x-10 translate-x-0 md:gap-5 gap-3  md:pl-15 grid  content-center justify-center">
-        <h1 className="md:text-5xl text-3xl font-bold">Your Tech Partner for </h1>
-        <h1 className="md:text-5xl text-3xl font-bold text-[#FF6C37]">Smarter Growth</h1>
-        <h4 className="md:text-xl text-sm">We deliver tailored IT solutions to help you scale with speed and confidence.</h4>
-        <Button className="w-full md:w-[197px]">Let's Talk</Button>    
+    <section
+      id="Hero"
+      className="h-[731px] bg-white dark:bg-black text-black dark:text-white 
+                 grid grid-cols-1 md:grid-cols-2 md:h-[663px]
+                 pt-24 md:pt-0" // Tambahkan padding atas untuk mengimbangi navbar
+    >
+      {/* Konten Hero: Article dan button */}
+      <article
+        className="p-4 w-full md:translate-x-10 translate-x-0
+                   md:pl-15 grid content-center justify-center
+                   gap-3 md:gap-5"
+      >
+        <hgroup>
+          <h1 className="md:text-5xl text-3xl font-bold">Your Tech Partner for</h1>
+          <h1 className="md:text-5xl text-3xl font-bold text-[#FF6C37]">Smarter Growth</h1>
+        </hgroup>
+        <p className="md:text-xl text-sm">We deliver tailored IT solutions to help you scale with speed and confidence.</p>
+        <Button className="w-full md:w-[197px]">Let's Talk</Button>
       </article>
-      {/* Image smartphone */}
-      <img className="dark:hidden block w-full h-auto object-contain md:h-[663px] md:w-[747px]" src="./public/2.Image Hero light.png" alt="smarphonelightmode" />
-      <img className="hidden dark:block w-full h-auto object-contain md:h-[663px] md:w-[747px]" src="./public/3.Image Hero dark.png" alt="smarphonelightmode" />
+
+      {/* Gambar Hero (Desktop & Mobile) */}
+      <div className="md:col-start-2 flex justify-center items-center">
+        <img
+          className="dark:hidden block w-full h-auto object-contain max-h-[663px] max-w-[747px]"
+          src="./public/2.Image Hero light.png"
+          alt="Smartphone in light mode"
+        />
+        <img
+          className="hidden dark:block w-full h-auto object-contain max-h-[663px] max-w-[747px]"
+          src="./public/3.Image Hero dark.png"
+          alt="Smartphone in dark mode"
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
--- src/components/container/05_Our_process/Our_process.tsx
+++ src/components/container/05_Our_process/Our_process.tsx
@@ -1,92 +1,104 @@
 import React from "react";
 
-const Our_process = () => {
+const OurProcess: React.FC = () => {
     return (
-    <section className="flex flex-col gap-16 py-20 max-[393px]:py-10 px-[140px] max-[393px]:px-3.5 bg-white dark:bg-black w-full">
-      <div className="flex flex-col gap-[11px] text-center">
-        {/* Text */}
-        <h2 className="max-[393px]:text-4xl max-[393px]:w-[361px] leading-14 text-[40px] font-bold text-black dark:text-white">Our Process</h2>
-        <p className="leading-8 text-[18px] font-medium text-[#717680]">
-          Clear steps. Smart execution. Results you can count on.
-        </p>
-      </div>
-      <div className="relative grid grid-cols-[1fr_48px_1fr] max-[393px]:grid-cols-[48px_1fr] gap-4 items-center">
-        {/* Line 1 */}
-        <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white max-[393px]:order-2">
-          <div className="flex flex-col cursor-pointer">
-            <p className="text-xl leading-[34px] font-bold">Discovery & Consultation</p>
-            <p className="leading-[30px] text-base font-medium text-[#717680]">Understand Your Needs & Goals</p>
-          </div>
-          <div className="basis-[12px]">
-            <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
-          </div>
-        </div>
-        <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-[393px]:order-1">1</div>
-        <div className="max-[393px]:hidden"></div>
-        {/* Line 2 */}
-        <div className="max-[393px]:hidden"></div>
-        <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-[393px]:order-3">2</div>
-        <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white max-[393px]:order-4">
-          <div className="flex flex-col cursor-pointer">
-            <p className="text-xl leading-[34px] font-bold">Planning & Strategy</p>
-            <p className="leading-[30px] text-base font-medium text-[#717680]">Build a Clear, Scalable Roadmap</p>
-          </div>
-          <div className="basis-[12px]">
-            <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
-          </div>
-        </div>
-        {/* Line 3 */}
-        <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white max-[393px]:order-6">
-          <div className="flex flex-col cursor-pointer">
-            <p className="text-xl leading-[34px] font-bold">Design & Prototyping</p>
-            <p className="leading-[30px] text-base font-medium text-[#717680]">Craft UX That Converts</p>
-          </div>
-          <div className="basis-[12px]">
-            <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
-          </div>
-        </div>
-        <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-[393px]:order-5">3</div>
-        <div className="max-[393px]:hidden"></div>
-        {/* Line 4 */}
-        <div className="max-[393px]:hidden"></div>
-        <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-[393px]:order-7">4</div>
-        <div className="cursor-pointer flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white max-[393px]:order-8">
-          <div className="flex flex-col">
-            <p className="text-xl leading-[34px] font-bold">Development & Implementation</p>
-            <p className="leading-[30px] text-base font-medium text-[#717680]">Deliver With Speed & Precision</p>
-          </div>
-          <div className="basis-[12px]">
-            <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
-          </div>
-        </div>
-        {/* Line 5 */}
-        <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white max-[393px]:order-10">
-          <div className="flex flex-col cursor-pointer">
-            <p className="text-xl leading-[34px] font-bold">Testing & Optimization</p>
-            <p className="leading-[30px] text-base font-medium text-[#717680]">Ensure Quality at Every Step</p>
-          </div>
-          <div className="basis-[12px]">
-            <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
-          </div>
-        </div>
-        <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-[393px]:order-9">5</div>
-        <div className="max-[393px]:hidden"></div>
-        {/* Line 6 */}
-        <div className="max-[393px]:hidden"></div>
-        <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-[393px]:order-11">6</div>
-        <div className="cursor-pointer flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white max-[393px]:order-12">
-          <div className="flex flex-col">
-            <p className="text-xl leading-[34px] font-bold">Launch & Growth</p>
-            <p className="leading-[30px] text-base font-medium text-[#717680]">Scale, Measure & Improve Continuously</p>
-          </div>
-          <div className="basis-[12px]">
-            <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
-          </div>
-        </div>
-        {/* Vertical Line */}
-        <div className="absolute top-[8.5%] left-1/2 max-[393px]:left-[24px] -z-0 w-[1px] h-10/12 bg-[#DEDCDC] dark:bg-[#181D27]"></div>
-      </div>
-    </section>
-    )
-}
-export default Our_process;
+        <section className="flex flex-col gap-16 py-20 px-[140px] bg-white dark:bg-black w-full overflow-x-hidden
+                          max-md:py-10 max-md:px-3.5">
+            <div className="flex flex-col gap-[11px] text-center">
+                {/* Text */}
+                <h2 className="text-[40px] font-bold text-black dark:text-white leading-14
+                               max-md:text-4xl max-md:w-[361px]">Our Process</h2>
+                <p className="leading-8 text-[18px] font-medium text-[#717680]">
+                    Clear steps. Smart execution. Results you can count on.
+                </p>
+            </div>
+            <div className="relative grid grid-cols-[1fr_48px_1fr] gap-4 items-center
+                          max-md:grid-cols-[48px_1fr]">
+                {/* Vertical Line */}
+                <div className="absolute top-[8.5%] left-1/2 -z-0 w-[1px] h-10/12 bg-[#DEDCDC] dark:bg-[#181D27]
+                              max-md:left-[24px]"></div>
+
+                {/* Line 1 */}
+                <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white
+                              max-md:order-2">
+                    <div className="flex flex-col cursor-pointer">
+                        <p className="text-xl leading-[34px] font-bold">Discovery & Consultation</p>
+                        <p className="leading-[30px] text-base font-medium text-[#717680]">Understand Your Needs & Goals</p>
+                    </div>
+                    <div className="basis-[12px]">
+                        <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
+                    </div>
+                </div>
+                <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-md:order-1">1</div>
+                <div className="max-md:hidden"></div>
+                {/* Line 2 */}
+                <div className="max-md:hidden"></div>
+                <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-md:order-3">2</div>
+                <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white
+                              max-md:order-4">
+                    <div className="flex flex-col cursor-pointer">
+                        <p className="text-xl leading-[34px] font-bold">Planning & Strategy</p>
+                        <p className="leading-[30px] text-base font-medium text-[#717680]">Build a Clear, Scalable Roadmap</p>
+                    </div>
+                    <div className="basis-[12px]">
+                        <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
+                    </div>
+                </div>
+                {/* Line 3 */}
+                <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white
+                              max-md:order-6">
+                    <div className="flex flex-col cursor-pointer">
+                        <p className="text-xl leading-[34px] font-bold">Design & Prototyping</p>
+                        <p className="leading-[30px] text-base font-medium text-[#717680]">Craft UX That Converts</p>
+                    </div>
+                    <div className="basis-[12px]">
+                        <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
+                    </div>
+                </div>
+                <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-md:order-5">3</div>
+                <div className="max-md:hidden"></div>
+                {/* Line 4 */}
+                <div className="max-md:hidden"></div>
+                <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-md:order-7">4</div>
+                <div className="cursor-pointer flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white
+                              max-md:order-8">
+                    <div className="flex flex-col">
+                        <p className="text-xl leading-[34px] font-bold">Development & Implementation</p>
+                        <p className="leading-[30px] text-base font-medium text-[#717680]">Deliver With Speed & Precision</p>
+                    </div>
+                    <div className="basis-[12px]">
+                        <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
+                    </div>
+                </div>
+                {/* Line 5 */}
+                <div className="flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white
+                              max-md:order-10">
+                    <div className="flex flex-col cursor-pointer">
+                        <p className="text-xl leading-[34px] font-bold">Testing & Optimization</p>
+                        <p className="leading-[30px] text-base font-medium text-[#717680]">Ensure Quality at Every Step</p>
+                    </div>
+                    <div className="basis-[12px]">
+                        <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
+                    </div>
+                </div>
+                <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-md:order-9">5</div>
+                <div className="max-md:hidden"></div>
+                {/* Line 6 */}
+                <div className="max-md:hidden"></div>
+                <div className="z-20 h-12 bg-[#ff623e] rounded-full leading-12 text-white text-center max-md:order-11">6</div>
+                <div className="cursor-pointer flex justify-between dark:bg-[#0A0D12] p-6 border dark:border-[#181D27] bg-[#FAFAFA] border-[#DEDCDC] rounded-2xl dark:text-white
+                              max-md:order-12">
+                    <div className="flex flex-col">
+                        <p className="text-xl leading-[34px] font-bold">Launch & Growth</p>
+                        <p className="leading-[30px] text-base font-medium text-[#717680]">Scale, Measure & Improve Continuously</p>
+                    </div>
+                    <div className="basis-[12px]">
+                        <img className="w-8 h-3 scale-200 text-[#0A0D12] filter invert dark:filter-none" src="assets/images/chevron-up.png" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default OurProcess;
